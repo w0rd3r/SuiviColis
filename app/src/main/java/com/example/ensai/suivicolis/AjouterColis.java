4efaa681c405aa07222e3e3b4779ccde46eb6c84
thelmacarrier <carrierthelma@gmail.com>
2016-05-10 12:20:05 +0200

    Activity Ajouter_Colis

---
+++ app/src/main/java/com/example/ensai/suivicolis/AjouterColis.java
@@ -0,0 +1,13 @@
+package com.example.ensai.suivicolis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AjouterColis extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ajouter_colis);
+    }
+}
