9b1434b1d1ba129fd44f7c38a25201ac76d6df88
thelmacarrier <carrierthelma@gmail.com>
2016-05-10 14:37:16 +0200

    CommetaireAjoutColis

--- app/src/main/java/com/example/ensai/suivicolis/AjouterColis.java
+++ app/src/main/java/com/example/ensai/suivicolis/AjouterColis.java
@@ -2,12 +2,24 @@ package com.example.ensai.suivicolis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class AjouterColis extends AppCompatActivity {
+public class AjouterColis extends AppCompatActivity implements View.OnClickListener{
+
+    Button boutonAjouterColis = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter_colis);
+
+        boutonAjouterColis = (Button) findViewById(R.id.boutonAjouterColis);
+        boutonAjouterColis.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
