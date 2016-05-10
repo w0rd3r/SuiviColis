f35ff51ef2e02013cbeed0e3cf90840ac80b1920
thelmacarrier <carrierthelma@gmail.com>
2016-05-10 14:59:15 +0200

    CommetaireAjoutColis

--- app/src/main/java/com/example/ensai/suivicolis/AjouterColis.java
+++ app/src/main/java/com/example/ensai/suivicolis/AjouterColis.java
@@ -2,12 +2,18 @@ package com.example.ensai.suivicolis;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.ensai.suivicolis.Core.Colis;
 
 public class AjouterColis extends AppCompatActivity implements View.OnClickListener{
 
     Button boutonAjouterColis = null;
+    EditText editTextDescription = null;
+    EditText editTextReference = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +22,19 @@ public class AjouterColis extends AppCompatActivity implements View.OnClickListe
 
         boutonAjouterColis = (Button) findViewById(R.id.boutonAjouterColis);
         boutonAjouterColis.setOnClickListener(this);
+
     }
 
     @Override
     public void onClick(View v) {
+        editTextDescription = (EditText) findViewById(R.id.edit_text_description);
+        String description = editTextDescription.getText().toString();
 
+        editTextReference = (EditText) findViewById(R.id.edit_text_reference);
+        String reference = editTextReference.getText().toString();
+
+
+        Colis newColis = new Colis();
+        newColis.setDescription(description);
     }
 }
